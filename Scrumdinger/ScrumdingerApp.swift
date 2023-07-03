d68b0c6689fc5726b3eb20c198e63d0d3da38d19
Satoshi Hirade <milktea.hm@gmail.com>
2023-07-03 21:22:56 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Satoshi Hirade on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
