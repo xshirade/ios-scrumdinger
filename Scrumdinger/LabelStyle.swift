e037c249c6ba4d045a8f045f5870ebb7d0b4058d
Satoshi Hirade <milktea.hm@gmail.com>
2023-07-09 16:01:46 +0900

    Finished 'Navigation Hierarchy'

---
+++ Scrumdinger/LabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  LabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Satoshi Hirade on 2023/07/03.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
